567d2ac3400b9607cdece28f0fdcd548d501117b
shubham1637 <shubham.1637@gmail.com>
2018-11-06 13:21:28 -0500

    Initialized alignment in C

---
+++ Alignment/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ROW_SIZE = 3;
+    int COL_SIZE = 4;
+
+    int i;
+    cout << "Hello !" << endl;
+    cin >> i;
+    int jj;
+    cout << "Hello World!" << endl;
+    cout << "The number is " << i+4 << endl;
+    return 0;
+}
+
---
+++ build-Alignment-Desktop-Default/CMakeFiles/Alignment.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Alignment.dir/main.cpp.o"
+  "Alignment.pdb"
+  "Alignment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Alignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
