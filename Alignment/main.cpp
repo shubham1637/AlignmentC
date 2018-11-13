3807bdb6ae65d12c244bd8fa621d8de104fef23a
shubham1637 <shubham.1637@gmail.com>
2018-11-13 00:26:31 -0500

    AffineAlignObj with RuleOf3, Traceback is missing though

--- Alignment/main.cpp
+++ Alignment/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <stdio.h>
+#include <cstring>
 #include <vector>
 #include <limits>
-
 #define NA -1
-using namespace std;
 
-void getseqSimMat(string seq1, string seq2, float Match, float MisMatch, float *s){
+//using namespace std;
+
+void getseqSimMat(std::string seq1, std::string seq2, float Match, float MisMatch, float *s){
     int ROW_SIZE = seq1.size();
     int COL_SIZE = seq2.size();
     for(int i = 0; i < ROW_SIZE; i++){
@@ -16,17 +17,17 @@ void getseqSimMat(string seq1, string seq2, float Match, float MisMatch, float *
     }
 }
 
-void printLengthOfSeq(string seq){
-    cout << "The length of sequence is " << seq.size() << endl;
+void printLengthOfSeq(std::string seq){
+    std::cout << "The length of sequence is " << seq.size() << std::endl;
 }
 
 template<class T>
 void printMatrix(T *s, int ROW_SIZE, int COL_SIZE){
     for(int i = 0; i < ROW_SIZE; i++){
         for(int j = 0; j < COL_SIZE; j++){
-            cout << *((s+i*COL_SIZE) + j) << " ";
+            std::cout << *((s+i*COL_SIZE) + j) << " ";
         }
-        cout << endl;
+        std::cout << std::endl;
     }
 }
 
@@ -63,12 +64,12 @@ void getOlapAlignStartIndices1(float *Matrix, int ROW_SIZE, int COL_SIZE, int &O
 
 template<class T>
 void getOlapAlignStartIndices(T &Matrix, int ROW_SIZE, int COL_SIZE, int &OlapStartRow, int &OlapStartCol){
-    cout << ROW_SIZE << " " << ROW_SIZE << endl;
+    std::cout << ROW_SIZE << " " << ROW_SIZE << std::endl;
     float maxScore = 0;
     int MaxRowIndex, MaxColIndex;
     for(int i = 0; i < ROW_SIZE; i++){
         if(Matrix[i][COL_SIZE-1] >= maxScore){
-            cout << Matrix[i][COL_SIZE-1]<< endl;
+            std::cout << Matrix[i][COL_SIZE-1]<< std::endl;
             MaxRowIndex = i;
             MaxColIndex = COL_SIZE-1;
             maxScore = Matrix[i][COL_SIZE-1];
@@ -90,15 +91,15 @@ const int ROW_SIZE = 3; // If I do not do const, it doesn't pass on to struct an
 const int COL_SIZE = 4; // that array bound is not an integer constant before ']'.
 
 struct AlignedIndices{
-    vector<int> indexA_aligned;
-    vector<int> indexB_aligned;
-    vector<float> score;
+    std::vector<int> indexA_aligned;
+    std::vector<int> indexB_aligned;
+    std::vector<float> score;
 };
 
 struct AlignObj
 {
-    vector< vector<char> > Traceback;
-    vector< vector<float> > M;
+    std::vector< std::vector<char> > Traceback;
+    std::vector< std::vector<float> > M;
     int signalA_len;
     int signalB_len;
     float Gap;
@@ -110,26 +111,74 @@ struct AffineAlignObj
     float* M;
     float* A;
     float* B;
-    string* Traceback;
-    AffineAlignObj(int ROW_SIZE, int COL_SIZE)
-    {
-        M = new float[ROW_SIZE * COL_SIZE];
-        A = new float[ROW_SIZE * COL_SIZE];
-        B = new float[ROW_SIZE * COL_SIZE];
-        Traceback = new string[3 * ROW_SIZE * COL_SIZE];
-    }
-    ~AffineAlignObj()
-    {
-        delete[] M;
-        delete[] A;
-        delete[] B;
-        delete[] Traceback;
-    }
-    int signalA_len;
+    //std::string* Traceback;
+    int signalA_len; // stack allocation
     int signalB_len;
     float GapOpen;
     float GapExten;
     bool FreeEndGaps;
+
+    // Not a default constructor
+    AffineAlignObj(int ROW_SIZE, int COL_SIZE)
+    {
+        M = new float[ROW_SIZE * COL_SIZE]; // heap allocation
+        A = new float[ROW_SIZE * COL_SIZE]; // heap allocation
+        B = new float[ROW_SIZE * COL_SIZE]; // heap allocation
+        //Traceback = new std::string[3*ROW_SIZE * COL_SIZE]; // heap allocation
+        signalA_len = ROW_SIZE-1;
+        signalB_len = COL_SIZE-1;
+    }
+
+    // Rule 1 Copy constructor
+    AffineAlignObj(const AffineAlignObj &other)
+       {
+          signalA_len = other.signalA_len;
+          signalB_len = other.signalB_len;
+          GapOpen = other.GapOpen;
+          GapExten = other.GapExten;
+          FreeEndGaps = other.FreeEndGaps;
+          M = new float[(signalA_len+1)*(signalB_len+1)];
+          std::memcpy(M, other.M, sizeof(float) * (signalA_len+1)*(signalB_len+1));
+          A = new float[(signalA_len+1)*(signalB_len+1)];
+          std::memcpy(A, other.A, sizeof(float) * (signalA_len+1)*(signalB_len+1));
+          B = new float[(signalA_len+1)*(signalB_len+1)];
+          std::memcpy(B, other.B, sizeof(float) * (signalA_len+1)*(signalB_len+1));
+//          Traceback = new std::string[3*(signalA_len+1)*(signalB_len+1)];
+//          std::memcpy(Traceback, other.Traceback, sizeof(std::string) * 3 * (signalA_len+1)*(signalB_len+1));
+       }
+
+    // Rule 2 Copy assignment operator
+    AffineAlignObj& operator=(const AffineAlignObj& other)
+    {
+        if(this == &other) return *this; // handling of self assignment.
+        delete[] M; // freeing previously used memory
+        delete[] A;
+        delete[] B;
+        // delete[] Traceback;
+        signalA_len = other.signalA_len;
+        signalB_len = other.signalB_len;
+        GapOpen = other.GapOpen;
+        GapExten = other.GapExten;
+        FreeEndGaps = other.FreeEndGaps;
+        M = new float[(signalA_len+1)*(signalB_len+1)];
+        std::memcpy(M, other.M, sizeof(float) * (signalA_len+1)*(signalB_len+1));
+        A = new float[(signalA_len+1)*(signalB_len+1)];
+        std::memcpy(A, other.A, sizeof(float) * (signalA_len+1)*(signalB_len+1));
+        B = new float[(signalA_len+1)*(signalB_len+1)];
+        std::memcpy(B, other.B, sizeof(float) * (signalA_len+1)*(signalB_len+1));
+        // Traceback = new std::string[3*(signalA_len+1)*(signalB_len+1)];
+        // std::memcpy(Traceback, other.Traceback, sizeof(std::string) * 3 * (signalA_len+1)*(signalB_len+1));
+        return *this;
+    }
+
+    // Rule 3 Not a default destructor
+    ~AffineAlignObj()
+    {
+        delete[] M; // since we declared with new, manually clear memory from heap.
+        delete[] A;
+        delete[] B;
+        // delete[] Traceback;
+    }
 };
 
 AlignedIndices getAlignedIndices(AlignObj alignObj){
@@ -231,14 +280,14 @@ AlignObj doAlignment(float *s, int signalA_len, int signalB_len, float gap, bool
         }
     }
 
-    cout << "M matrix is : " << endl;
+    std::cout << "M matrix is : " << std::endl;
     printMatrix((float *)M, signalA_len+1, signalB_len+1);
     printMatrix((char *)Traceback, signalA_len+1, signalB_len+1);
 
     // Copy cumulative score matrix(M) and traceback matrix into a instance of AlignObj.
     for (int i = 0; i < signalA_len+1; i++) {
-        vector<float> rowF; // Create an empty row
-        vector<char> rowC; // Create an empty row
+        std::vector<float> rowF; // Create an empty row
+        std::vector<char> rowC; // Create an empty row
         for (int j = 0; j < signalB_len+1; j++) {
             rowF.push_back(M[i][j]); // Add an element (column) to the row
             rowC.push_back(Traceback[i][j]); // Add an element (column) to the row
@@ -250,140 +299,123 @@ AlignObj doAlignment(float *s, int signalA_len, int signalB_len, float gap, bool
     return alignObj;
 }
 
-AffineAlignObj doAffineAlignment(float *s, int signalA_len, int signalB_len, float go, float ge, bool OverlapAlignment){
-    AffineAlignObj affineAlignObj(signalA_len+1, signalB_len+1);
+void doAffineAlignment(float *s, int signalA_len, int signalB_len, float go, float ge, bool OverlapAlignment, AffineAlignObj &affineAlignObj){
     affineAlignObj.FreeEndGaps = OverlapAlignment;
     affineAlignObj.GapOpen = go;
     affineAlignObj.GapExten = ge;
-    affineAlignObj.signalA_len = signalA_len;
-    affineAlignObj.signalB_len = signalB_len;
-
-    float M[signalA_len+1][signalB_len+1];
-    initializeMatrix(*M, 0, signalA_len+1, signalB_len+1);
-
-    float A[signalA_len+1][signalB_len+1];
-    initializeMatrix(*A, 0, signalA_len+1, signalB_len+1);
-
-    float B[signalA_len+1][signalB_len+1];
-    initializeMatrix(*B, 0, signalA_len+1, signalB_len+1);
 
     // enum TbPointer{STOP='S', T='T', D='D', L='L'};
-    string Traceback[3][signalA_len+1][signalB_len+1]; // Traceback[0,1,2] = Traceback[TrM,TrA,TrB]
+    std::string Traceback[3][signalA_len+1][signalB_len+1]; // Traceback[0,1,2] = Traceback[TrM,TrA,TrB]
     //initializeMatrix(*Traceback, 'S', signalA_len+1, signalB_len+1);
 
     // Initialize first row and first column for global and overlap alignment.
     float Inf = std::numeric_limits<float>::infinity();
     for(int i = 0; i<=signalA_len; i++){
-        M[i][0] = -Inf;
-        B[i][0] = -Inf;
+        *(affineAlignObj.M+i*(signalB_len+1)+0) = -Inf;
+        *(affineAlignObj.B+i*(signalB_len+1)+0) = -Inf;
         Traceback[0][i][0] = "SS"; //STOP
         Traceback[2][i][0] = "SS"; //STOP
     }
     for(int j = 0; j<=signalB_len; j++){
-        M[0][j] = -Inf;
-        A[0][j] = -Inf;
+        *(affineAlignObj.M+0*(signalB_len+1)+j) = -Inf;
+        *(affineAlignObj.A+0*(signalB_len+1)+j) = -Inf;
         Traceback[0][0][j] = "SS"; //STOP
         Traceback[1][0][j] = "SS"; //STOP
     }
-    M[0][0] = 0;
+    *(affineAlignObj.M+0*(signalB_len+1)+0) = 0;
     if(affineAlignObj.FreeEndGaps == true){
         for(int i = 1; i<=signalA_len; i++){
-            A[i][0] = 0;
+            *(affineAlignObj.A+i*(signalB_len+1)+0) = 0;
             Traceback[1][i][0] = "TA"; //TOP A
         }
         for(int j = 1; j<=signalB_len; j++){
-            B[0][j] = 0;
+            *(affineAlignObj.B+0*(signalB_len+1)+j) = 0;
             Traceback[2][0][j] = "LB"; //LEFT B
         }
     } else {
         for(int i = 1; i<=signalA_len; i++){
-            A[i][0] = -(i-1)*ge - go;
+            *(affineAlignObj.A+i*(signalB_len+1)+0) = -(i-1)*ge - go;
             Traceback[1][i][0] = "TA"; //TOP A
         }
         for(int j = 1; j<=signalB_len; j++){
-            B[0][j] = -(j-1)*ge - go;
+            *(affineAlignObj.B+0*(signalB_len+1)+j) = -(j-1)*ge - go;
             Traceback[2][0][j] = "LB"; //LEFT B
         }
     }
 
     // Perform dynamic programming for affine alignment
     float Diago, gapInA, gapInB;
-    for(int j=1; j<=signalB_len; j++ ){
-        for(int i=1; i<=signalA_len; i++){
+    for(int i=1; i<=signalA_len; i++){
+        for(int j=1; j<=signalB_len; j++){
             float sI_1J_1 = *((s+(i-1)*signalB_len) + j-1);
-            Diago = M[i-1][j-1] + sI_1J_1;
-            gapInA = A[i-1][j-1] + sI_1J_1;
-            gapInB = B[i-1][j-1] + sI_1J_1;
+            Diago = *(affineAlignObj.M+(i-1)*(signalB_len+1)+j-1) + sI_1J_1;
+            gapInA = *(affineAlignObj.A+(i-1)*(signalB_len+1)+j-1) + sI_1J_1;
+            gapInB = *(affineAlignObj.B+(i-1)*(signalB_len+1)+j-1) + sI_1J_1;
 
             // Calculate recursively for matched alignment
             if(Diago>=gapInA && Diago>=gapInB){
                 Traceback[0][i][j] = "DM"; // DM: Diagonal TrM
-                M[i][j] = Diago;
+                *(affineAlignObj.M+i*(signalB_len+1)+j) = Diago;
             }
             else if (gapInA>=Diago && gapInA>=gapInB){
                 Traceback[0][i][j] = "DA"; // DA: Diagonal TrA
-                M[i][j] = gapInA;
+                *(affineAlignObj.M+i*(signalB_len+1)+j) = gapInA;
             }
             else{
                 Traceback[0][i][j] = "DB"; // DB: Diagonal TrB
-                M[i][j] = gapInB;
+                *(affineAlignObj.M+i*(signalB_len+1)+j) = gapInB;
             }
 
             // Calculate recursively for gap in signalB
-            if((M[i-1][j]-go) >= (A[i-1][j]-ge) && (M[i-1][j]-go) >= (B[i-1][j]-go)){
+            float AfromM = *(affineAlignObj.M+(i-1)*(signalB_len+1)+j) - go;
+            float AfromA = *(affineAlignObj.A+(i-1)*(signalB_len+1)+j) - ge;
+            float AfromB = *(affineAlignObj.B+(i-1)*(signalB_len+1)+j) - go;
+            if(AfromM >= AfromA && AfromM >= AfromB){
                 Traceback[1][i][j] = "TM"; // TM: Top TrM
-                A[i][j] = M[i-1][j]-go;
+                *(affineAlignObj.A+i*(signalB_len+1)+j) = AfromM;
             }
-            else if ((A[i-1][j]-ge) >= (M[i-1][j]-go) && (A[i-1][j]-ge) >= (B[i-1][j]-go)){
+            else if (AfromA >= AfromM && AfromA >= AfromB){
                 Traceback[1][i][j] = "TA"; // TA: Top TrA
-                A[i][j] = A[i-1][j]-ge;
+                *(affineAlignObj.A+i*(signalB_len+1)+j) = AfromA;
             }
             else{
                 Traceback[1][i][j] = "TB"; // TB: Top TrB
-                A[i][j] = B[i-1][j]-go;
+                *(affineAlignObj.A+i*(signalB_len+1)+j) = AfromB;
             }
 
             // Calculate recursively for gap in signalA
-            if((M[i][j-1]-go) >= (A[i][j-1]-go) && (M[i][j-1]-go) >= (B[i][j-1]-ge)){
+            float BfromM = *(affineAlignObj.M+i*(signalB_len+1)+j-1) - go;
+            float BfromA = *(affineAlignObj.A+i*(signalB_len+1)+j-1) - go;
+            float BfromB = *(affineAlignObj.B+i*(signalB_len+1)+j-1) - ge;
+            if(BfromM >= BfromA && BfromM >= BfromB){
                 Traceback[2][i][j] = "LM"; // TM: Top TrM
-                B[i][j] = M[i][j-1]-go;
+                *(affineAlignObj.B+i*(signalB_len+1)+j) = BfromM;
             }
-            else if ((A[i][j-1]-go) >= (M[i][j-1]-go) && (A[i][j-1]-go) >= (B[i][j-1]-ge)){
+            else if (BfromA >= BfromM && BfromA >= BfromB){
                 Traceback[2][i][j] = "LA"; // TA: Top TrA
-                B[i][j] = A[i][j-1]-go;
+                *(affineAlignObj.B+i*(signalB_len+1)+j) = BfromA;
             }
             else{
                 Traceback[2][i][j] = "LB"; // TB: Top TrB
-                B[i][j] = B[i][j-1]-ge;
+                *(affineAlignObj.B+i*(signalB_len+1)+j) = BfromB;
             }
         }
     }
 
-    cout << "M matrix is : " << endl;
-    printMatrix((float *)M, signalA_len+1, signalB_len+1);
-    printMatrix((float *)A, signalA_len+1, signalB_len+1);
-    printMatrix((float *)B, signalA_len+1, signalB_len+1);
     // printMatrix((string *)Traceback[0], signalA_len+1, signalB_len+1);
-
-    affineAlignObj.M = *M;
-    affineAlignObj.A = *A;
-    affineAlignObj.B = *B;
-    affineAlignObj.Traceback = **Traceback;
-    cout << "IN the loop " << endl;
-    return affineAlignObj;
 }
 
 int main()
 {
     float Match=10, MisMatch=-2, go=22, ge=7, gap=go;
-    string seq1 = "GCAT";
-    string seq2 = "CAGTG";
+    std::string seq1 = "GCAT";
+    std::string seq2 = "CAGTG";
     int seq1Len = seq1.size();
     int seq2Len = seq2.size();
     float s[seq1Len][seq2Len];
     initializeMatrix(*s, 0, seq1Len, seq2Len);
     getseqSimMat(seq1, seq2, Match, MisMatch, &s[0][0]); // getseqSimMat(seq1, seq2, Match, MisMatch, (float *)s);
-    cout << "Similarity matrix is : " << endl;
+    std::cout << "Similarity matrix is : " << std::endl;
     printMatrix(*s, seq1Len, seq2Len);
     int signalA_len = sizeof(s) / sizeof(s[0]);
     int signalB_len = sizeof(s[0]) / sizeof(s[0][0]);
@@ -392,13 +424,22 @@ int main()
     AlignObj alignObj;
     alignObj = doAlignment(*s, seq1Len, seq2Len, gap, OverlapAlignment);
     AffineAlignObj affineAlignObj(seq1Len+1, seq2Len+1); // What if this length is different than used inside the function.
-    affineAlignObj = doAffineAlignment(*s, seq1Len, seq2Len, go, ge, OverlapAlignment);
-    cout << "Out the loop " << endl;
+    doAffineAlignment(*s, seq1Len, seq2Len, go, ge, OverlapAlignment, affineAlignObj);
+    std::cout << "Out the loop " << std::endl;
 
-    cout << "M matrix is : " << endl;
+    AffineAlignObj affineAlignObj2(seq1Len+1, seq2Len+1);
+    affineAlignObj2 = affineAlignObj;
+    doAffineAlignment(*s, seq1Len, seq2Len, 3, 3, OverlapAlignment, affineAlignObj);
+
+    std::cout << "M matrix is : " << std::endl;
     printMatrix((float *)affineAlignObj.M, signalA_len+1, signalB_len+1);
     printMatrix((float *)affineAlignObj.A, signalA_len+1, signalB_len+1);
     printMatrix((float *)affineAlignObj.B, signalA_len+1, signalB_len+1);
+
+    std::cout << "M matrix is : " << std::endl;
+    printMatrix((float *)affineAlignObj2.M, signalA_len+1, signalB_len+1);
+    printMatrix((float *)affineAlignObj2.A, signalA_len+1, signalB_len+1);
+    printMatrix((float *)affineAlignObj2.B, signalA_len+1, signalB_len+1);
 
     return 0;
 }
