2bf77383cc6cd72135914b5a1800748eaaa7d329
Hannes Roest <hannes.rost@utoronto.ca>
2019-04-16 21:40:35 -0400

    [DOC] add docu

--- Alignment/alignment.h
+++ Alignment/alignment.h
@@ -26,11 +26,28 @@ struct AlignObj
 
 AlignedIndices getAlignedIndices(AlignObj alignObj);
 
+/** @brief Perform (simple) alignment on similarly matrix s
+ *
+ * @param s Input similarly matrix
+ * @param signalA_len The number of rows
+ * @param signalB_len The number of columns
+ * @param gap The gap penalty
+ * @param OverlapAlignment Whether end gaps should be free
+ *
+ * @return AlignObj with filled out matrix M and Traceback
+ */
 AlignObj doAlignment(float *s, int signalA_len, int signalB_len, float gap, bool OverlapAlignment);
 
 template<class T>
 void getOlapAlignStartIndices(T &Matrix, int ROW_SIZE, int COL_SIZE, int &OlapStartRow, int &OlapStartCol);
 
+/** @brief Initializes matrix with a given value
+ *
+ * @param T1 A pointer to the matrix to initialize
+ * @param T2 The value to use for initialization
+ * @param ROW_SIZE The number of rows
+ * @param COL_SIZE The number of columns
+ */
 template<class T1, class T2> // Don't know why do I have to specify this line everytime. It is just the rule.
 void initializeMatrix(T1 *s, T2 val, int ROW_SIZE, int COL_SIZE);
 
